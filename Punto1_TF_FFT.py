0ff90d491b870b068c9a93b80a25635a693acee6
danielherreracorrea <93483425+danielherreracorrea@users.noreply.github.com>
2021-11-22 17:17:37 -0500

    Some mistakes
    I've corrected minor mistakes in the written code (a bad position parentheses in the function) and changed the dimension of th physical array (it's now in nm)

--- Punto1_TF_FFT.py
+++ Punto1_TF_FFT.py
@@ -14,24 +14,24 @@ import cv2
 
 
 
-def Fresnel(Uin, w_l, f_l, dx0, z):
+def Fresnel(Uin, w_l, dx0, z):
     "-----Step 1------"
     k=2*np.pi/w_l
     N,M=np.shape(Uin)
     x=np.arange(-N/2,N/2,1)
     y=np.arange(-M/2,M/2,1)
     X,Y=np.meshgrid(x,y)
-    phase=np.exp(1j*(k/2*z)*(((X*dx0)**2) + ((Y*dx0)**2)))
+    phase=np.exp((1j*k)/(2*z)*(((X*dx0)**2) + ((Y*dx0)**2)))
     U1=Uin*phase
     "-----Step 2-----"
     dx=w_l*z/(dx0*N)
     print (dx)
-    X=X*dx
-    Y=Y*dx
     #X=X*(1/(M*dx0))
     #Y=Y*(1/(N*dx0))
-    Uf=np.fft.fftshift(np.fft.fft2(U1))
+    Uf=np.fft.fftshift(np.fft.fft2(U1*dx0**2))
     "-----Step 3-----"
+    X=X*dx
+    Y=Y*dx
     c1=np.exp(1j*k*z)/(1j*w_l*z)
     Uf=Uf*c1*np.exp(1j*(k/2*z)*((X)**2 + (Y)**2))
     
@@ -39,12 +39,11 @@ def Fresnel(Uin, w_l, f_l, dx0, z):
 
 "-----Physical array-----"
         
-f_l=50000*1e-6            #(50mm)
-w_l=0.633*1e-6            #(633nm orange/red) #All units in um
-dx0=2.5*1e-6
+w_l=633          #(633nm orange/red) #All units in um
+dx0=2000
 N=M=int(512/2)
-z=1000*N*(dx0**2)/w_l  #Condition of z in FT
-
+#z=1*N*(dx0**2)/w_l  #Condition of z in FT
+z=2500000
 U_0=cv2.imread('cameraman.png',0)
 
 "-----PADDING-----" #If needed 
@@ -54,8 +53,8 @@ r=int(512/2)
 U_0 = cv2.copyMakeBorder(U_0,r,r,r,r,cv2.BORDER_CONSTANT)"""
 """ FINALIZA PADDING """
 
-
-Uf=Fresnel(U_0, w_l, f_l, dx0, z)
+print (z)
+Uf=Fresnel(U_0, w_l, dx0, z)
 I1=np.log(np.abs((Uf)**2))
 
 plt.figure(1)
