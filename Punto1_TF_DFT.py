e910e770f52a054c172d66bb0bca9266c16e7ba0
danielherreracorrea <93483425+danielherreracorrea@users.noreply.github.com>
2021-11-22 19:30:32 -0500

    i'm tired

--- Punto1_TF_DFT.py
+++ Punto1_TF_DFT.py
@@ -12,6 +12,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
+def Umatrix(z, w_length, deltax0, deltay0, M,N):
+    """
+    Incident wave and transmittance function
+    In this case: plane wave and circular aperture 
+    """
+    x=np.arange(-M,M)
+    y=np.arange(-N,N)
+    x,y=np.meshgrid(x,y)
+    lim=10*w_length*z
+    U_matrix=(deltax0*x)**2 + (deltay0*y)**2
+    U_matrix[np.where(U_matrix<=lim)]=1
+    U_matrix[np.where(U_matrix>lim)]=0
+
+    return U_matrix
 
 def DFT(Uin, dx0, w_l):
     N,M=np.shape(Uin)
@@ -22,12 +36,12 @@ def DFT(Uin, dx0, w_l):
     j=np.arange(-M/2,M/2,1)
     I,J=np.meshgrid(i,j)
     dx=w_l*z/(dx0*N)
-    Uf=(dx**2)*Uin*np.exp((-1j*2*np.pi/N)*(I*X+J*Y))
-    #Uf=0
-    #for i in range(-int(N/2),int(N/2) ,1):
-    #    print (i)
-    #    for j in range(-int(M/2),int(M/2),1):
-    #        Uf=(dx**2)*Uin*np.exp((-1j*2*np.pi/N)*(i*X+j*Y)) + Uf
+    #Uf=(dx**2)*Uin*np.exp((-1j*2*np.pi/N)*(I*X+J*Y))
+    Uf=0
+    for i in range(-int(N/2),int(N/2) ,1):
+        print (i)
+        for j in range(-int(M/2),int(M/2),1):
+            Uf=(dx**2)*Uin*np.exp((-1j*2*np.pi/N)*(i*X+j*Y)) + Uf
     return Uf
 
 
@@ -64,8 +78,8 @@ dx0=2000        #2um
 N=M=int(512/2)
 #z=1*N*(dx0**2)/w_l  #Condition of z in FT
 z=33*1e5   #3.2 mm
-U_0=cv2.imread('cameraman.png',0)
-
+#U_0=cv2.imread('cameraman.png',0)
+U_0=Umatrix(z, w_l, dx0, dx0, M, N )
 "-----PADDING-----" #If needed 
 """
 width=height=512
@@ -82,5 +96,5 @@ plt.imshow(U_0, extent=[-N,N,-N,N])
 
 
 plt.figure(2)
-plt.imshow(I1)
+plt.imshow(I1, cmap='gray')
 plt.imsave("TF.png",I1, cmap='gray')
