01f2635df4a164fc8242e5efdd7c5cf116aaf88e
alegomezram <alegomezram@unal.edu.co>
2021-11-24 18:33:42 -0500

    Fresnel FFT code added to see number of Fresnel zones

--- Punto1_TF_FFT.py
+++ Punto1_TF_FFT.py
@@ -1,5 +1,5 @@
 """
-Fresnel Transform using TTF
+Fresnel Transform using FFT
 
 ESQUEMA
 1. Calculate U'[n,m,0] multiplying by a phase
@@ -11,10 +11,11 @@ ESQUEMA
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+import time
 
 
 
-def Fresnel(Uin, w_l, dx0, z):
+def Fresnel(Uin, w_l, dx0, dx, z):
     "-----Step 1------"
     k=2*np.pi/w_l
     N,M=np.shape(Uin)
@@ -24,27 +25,37 @@ def Fresnel(Uin, w_l, dx0, z):
     phase=np.exp((1j*k)/(2*z)*(((X*dx0)**2) + ((Y*dx0)**2)))
     U1=Uin*phase
     "-----Step 2-----"
-    dx=w_l*z/(dx0*N)
-    print (dx)
     #X=X*(1/(M*dx0))
     #Y=Y*(1/(N*dx0))
     Uf=np.fft.fftshift(np.fft.fft2(U1*dx0**2))
     "-----Step 3-----"
-    X=X*dx
-    Y=Y*dx
+
     c1=np.exp(1j*k*z)/(1j*w_l*z)
-    Uf=Uf*c1*np.exp(1j*(k/2*z)*((X)**2 + (Y)**2))
+    Uf=Uf*c1*(np.exp((1j*(k/2*z))*((X*dx)**2 + (Y*dx)**2)))
     
     return Uf
 
 "-----Physical array-----"
+
+"""
+U=incident wave
+z=entrance plane to detector plane distance
+w_l= wavelentgth
+dx=dy=pixel size detector plane
+dx0=dy0=pixel size entrance plane
+M=number of pixels in the x axis
+N=number of pixels in the y axis
+(M=N)
+MxN=number of pixels
+"""
         
-w_l=633          #(633nm orange/red) #All units in nm
-dx0=2000        #2nm
-N=M=int(512/2)
-#z=1*N*(dx0**2)/w_l  #Condition of z in FT
-z=33*1e5   #3.2 mm
+w_l=0.633          #(633nm orange/red)   #All units in um
+dx0=2              #2um
+N=M=int(512/2)     #Number of pixels
+z=3500             #2.5 mm
 U_0=cv2.imread('cameraman.png',0)
+
+tic=time.time()
 
 "-----PADDING-----" #If needed 
 """
@@ -53,16 +64,33 @@ r=int(512/2)
 U_0 = cv2.copyMakeBorder(U_0,r,r,r,r,cv2.BORDER_CONSTANT)"""
 """ FINALIZA PADDING """
 
-print (z)
-Uf=Fresnel(U_0, w_l, dx0, z)
-I1=np.log(np.abs((Uf)**2))
+
+dx=w_l*z/(dx0*N)
+print ("dx:",dx)
+lim=N*(dx0**2)/w_l  #Limit of z in FT
+if z>=lim:
+    print("z limit exceeded")
+
+Uf=Fresnel(U_0, w_l, dx0, dx, z)
+
+
+I=(np.abs(Uf)**2)                            #Intensity
+angle=np.angle(Uf)                           #Phase
+
+x=N*dx
+y=N*dx
 
 plt.figure(1)
-plt.imshow(U_0, extent=[-N,N,-N,N])
-
+plt.imshow(I, extent=[-x,x,-y,y])
 
 plt.figure(2)
-plt.imshow(I1)
-plt.imsave("TF.png",I1, cmap='gray')
+plt.imshow(I)
+plt.imsave("FresnelInt.png",I, cmap='gray')
 
+plt.figure(2)
+plt.imshow(angle)
+plt.imsave("FresnelPhase.png",angle, cmap='gray')
+
+toc=time.time()
+print("time: ",toc-tic," sec")
 
--- Punto1_TF_FFT_Fresnelzones.py
+++ Punto1_TF_FFT_Fresnelzones.py
@@ -11,10 +11,26 @@ ESQUEMA
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+import time
 
+def Umatrix(z, w_l, dx0, N):
+    """
+    Incident wave and transmittance function
+    In this case: plane wave and circular aperture 
+    """
+    dy0=dx0
+    x=np.arange(-N/2,N/2)
+    y=np.arange(-N/2,N/2)
+    x,y=np.meshgrid(x,y)
+    Nzones=8       #Number of Fresnel zones
+    lim=Nzones*w_l*z
+    U_matrix=(dx0*x)**2 + (dy0*y)**2
+    U_matrix[np.where(U_matrix<=lim)]=1
+    U_matrix[np.where(U_matrix>lim)]=0
 
+    return U_matrix
 
-def Fresnel(Uin, w_l, dx0, z):
+def Fresnel(Uin, w_l, dx0, dx, z):
     "-----Step 1------"
     k=2*np.pi/w_l
     N,M=np.shape(Uin)
@@ -24,8 +40,6 @@ def Fresnel(Uin, w_l, dx0, z):
     phase=np.exp((1j*k)/(2*z)*(((X*dx0)**2) + ((Y*dx0)**2)))
     U1=Uin*phase
     "-----Step 2-----"
-    dx=w_l*z/(dx0*N)
-    print (dx)
     #X=X*(1/(M*dx0))
     #Y=Y*(1/(N*dx0))
     Uf=np.fft.fftshift(np.fft.fft2(U1*dx0**2))
@@ -49,20 +63,13 @@ N=number of pixels in the y axis
 (M=N)
 MxN=number of pixels
 """
-
-N=1024 #Number of pixels=M*2
-z=2500000  #(2.5mm)
-w_length=633   #All units in nm
-deltax0=2000 
-deltay0=2000
-
         
-w_l=633          #(633nm orange/red) #All units in um
-dx0=2000        #2um
-N=M=int(512/2)
-#z=1*N*(dx0**2)/w_l  #Condition of z in FT
-z=33*1e5   #3.2 mm
-U_0=cv2.imread('cameraman.png',0)
+w_l=0.633          #(633nm orange/red)   #All units in um
+dx0=2              #2um
+N=M=2048           #Number of pixels
+z=3500             #2.5 mm
+
+tic=time.time()
 
 "-----PADDING-----" #If needed 
 """
@@ -71,16 +78,34 @@ r=int(512/2)
 U_0 = cv2.copyMakeBorder(U_0,r,r,r,r,cv2.BORDER_CONSTANT)"""
 """ FINALIZA PADDING """
 
-print (z)
-Uf=Fresnel(U_0, w_l, dx0, z)
-I1=np.log(np.abs((Uf)**2))
+
+dx=w_l*z/(dx0*N)
+print ("dx:",dx)
+lim=N*(dx0**2)/w_l  #Limit of z in FT
+if z>=lim:
+    print("z limit exceeded")
+
+U=Umatrix(z, w_l, dx0, N)
+Uf=Fresnel(U, w_l, dx0, dx, z)
+
+
+I=(np.abs(Uf)**2)                            #Intensity
+angle=np.angle(Uf)                           #Phase
+
+x=N*dx
+y=N*dx
 
 plt.figure(1)
-plt.imshow(U_0, extent=[-N,N,-N,N])
-
+plt.imshow(I, extent=[-x,x,-y,y])
 
 plt.figure(2)
-plt.imshow(I1)
-plt.imsave("TF.png",I1, cmap='gray')
+plt.imshow(I)
+plt.imsave("FresnelInt.png",I, cmap='gray')
 
+plt.figure(2)
+plt.imshow(angle)
+plt.imsave("FresnelPhase.png",angle, cmap='gray')
+
+toc=time.time()
+print("time: ",toc-tic," sec")
 
