55eef0d224d3341b56fd8e500d70b8d405b2c7d6
alegomezram <alegomezram@unal.edu.co>
2021-11-24 18:12:10 -0500

    Addition of z limit
    The limit of the z measurement constrain is added

--- Punto1_TF_FFT_Fresnelzones.py
+++ Punto1_TF_FFT_Fresnelzones.py
@@ -11,11 +11,10 @@ ESQUEMA
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-import time
 
 
 
-def Fresnel(Uin, w_l, dx0, dx, z):
+def Fresnel(Uin, w_l, dx0, z):
     "-----Step 1------"
     k=2*np.pi/w_l
     N,M=np.shape(Uin)
@@ -25,6 +24,8 @@ def Fresnel(Uin, w_l, dx0, dx, z):
     phase=np.exp((1j*k)/(2*z)*(((X*dx0)**2) + ((Y*dx0)**2)))
     U1=Uin*phase
     "-----Step 2-----"
+    dx=w_l*z/(dx0*N)
+    print (dx)
     #X=X*(1/(M*dx0))
     #Y=Y*(1/(N*dx0))
     Uf=np.fft.fftshift(np.fft.fft2(U1*dx0**2))
@@ -48,14 +49,20 @@ N=number of pixels in the y axis
 (M=N)
 MxN=number of pixels
 """
-        
-w_l=0.633          #(633nm orange/red)   #All units in um
-dx0=2              #2um
-N=M=int(512/2)     #Number of pixels
-z=3500             #2.5 mm
-U_0=cv2.imread('cameraman.png',0)
 
-tic=time.time()
+N=1024 #Number of pixels=M*2
+z=2500000  #(2.5mm)
+w_length=633   #All units in nm
+deltax0=2000 
+deltay0=2000
+
+        
+w_l=633          #(633nm orange/red) #All units in um
+dx0=2000        #2um
+N=M=int(512/2)
+#z=1*N*(dx0**2)/w_l  #Condition of z in FT
+z=33*1e5   #3.2 mm
+U_0=cv2.imread('cameraman.png',0)
 
 "-----PADDING-----" #If needed 
 """
@@ -64,33 +71,16 @@ r=int(512/2)
 U_0 = cv2.copyMakeBorder(U_0,r,r,r,r,cv2.BORDER_CONSTANT)"""
 """ FINALIZA PADDING """
 
-
-dx=w_l*z/(dx0*N)
-print ("dx:",dx)
-lim=N*(dx0**2)/w_l  #Limit of z in FT
-if z>=lim:
-    print("z limit exceeded")
-
-Uf=Fresnel(U_0, w_l, dx0, dx, z)
-
-
-I=(np.abs(Uf)**2)                            #Intensity
-angle=np.angle(Uf)                           #Phase
-
-x=N*dx
-y=N*dx
+print (z)
+Uf=Fresnel(U_0, w_l, dx0, z)
+I1=np.log(np.abs((Uf)**2))
 
 plt.figure(1)
-plt.imshow(I, extent=[-x,x,-y,y])
+plt.imshow(U_0, extent=[-N,N,-N,N])
+
 
 plt.figure(2)
-plt.imshow(I)
-plt.imsave("FresnelInt.png",I, cmap='gray')
+plt.imshow(I1)
+plt.imsave("TF.png",I1, cmap='gray')
 
-plt.figure(2)
-plt.imshow(angle)
-plt.imsave("FresnelPhase.png",angle, cmap='gray')
-
-toc=time.time()
-print("time: ",toc-tic," sec")
 
